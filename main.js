368d460230c52b3e17e2969370f5dcd1427818f4
Vijay Bharath V <123536778+VijayFencer@users.noreply.github.com>
2024-06-22 20:50:49 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,10 +1,10 @@
 const panoramaImage = new PANOLENS.ImagePanorama("Amul.jpg");
 const imageContainer = document.querySelector(".image-container");
-//Panolens IMage Viewer
+//Panolens Image Viewer
 const viewer = new PANOLENS.Viewer({
   container: imageContainer,
  
-  autoRotateSpeed: 0.3,
+  autoRotateSpeed: 0.3,//image autorotation speed
   controlBar: false,
 });
 
